fc6a02aba3f5a4eb01823f3e88414e61248ac747
Jemimah Mmboga <mmbogajemimah@gmail.com>
2024-12-01 18:57:06 +0300

    Complex Queries with Joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -5,9 +5,26 @@ INNER JOIN Users ON Bookings.user_id = Users.id;
 
 
 -- Write a query using aLEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
-SELECT *
-FROM Property
-LEFT JOIN Reviews ON Property.id = Reviews.property_id;
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    p.description AS property_description,
+    p.location AS property_location,
+    p.pricepernight,
+    p.created_at AS property_created_at,
+    p.updated_at AS property_updated_at,
+    r.review_id,
+    r.rating,
+    r.comment,
+    r.created_at AS review_created_at
+FROM 
+    Property p
+LEFT JOIN 
+    Review r
+ON 
+    p.property_id = r.property_id
+ORDER BY 
+    p.property_id, r.created_at;
 
 
 
