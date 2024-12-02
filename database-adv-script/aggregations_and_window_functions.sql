8e16cf2e6150203f11b2456d918b07119a7dc69e
Jemimah Mmboga <mmbogajemimah@gmail.com>
2024-12-02 20:13:17 +0300

    Advanced Querying Power

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,29 +1,25 @@
-SELECT 
-    b.user_id,
-    u.first_name,
-    u.last_name,
-    COUNT(b.booking_id) AS total_bookings
-FROM 
-    Booking b
-JOIN 
-    User u ON b.user_id = u.user_id
-GROUP BY 
-    b.user_id, u.first_name, u.last_name
-ORDER BY 
-    total_bookings DESC;
+-- Write a query to find the total number of bookings made by each user, using the COUNT function and GROUP BY clause.
+
+SELECT guest_id, COUNT(*) AS total_bookings
+FROM Bookings
+GROUP BY guest_id;
 
 
+
+-- Use a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received.
+-- Query using ROW_NUMBER and RANK window functions
 
 SELECT 
     p.property_id,
     p.name,
     COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS row_number_rank,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS rank
 FROM 
-    Property p
-JOIN 
-    Booking b ON p.property_id = b.property_id
+    Properties p
+LEFT JOIN 
+    Bookings b ON p.property_id = b.property_id
 GROUP BY 
     p.property_id, p.name
 ORDER BY 
-    booking_rank;
+    row_number_rank;
